25f740963eaebaa03984a89b8fed0a1cffa248f0
Kathy <Kathy@Kathy-Giebelhausens-iMac.local>
2018-02-23 16:49:45 -0700

    is this right?

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+// define all you variable that will be used in the game
+
+// targetNumber 
+// wins
+// loseses
+// resultNumber
+
+
+// display variable value on the page
+
+// assign value to gems
+
+// create a function that take two paramiters : a min and a max
+// 	this function should then take the min and max and generate a random number between the two numbers
+
+// 	look at the second reply with 1539 score
+// 	https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+
+
+// check what value of gem is for clicked gem 
+// update resultNumber and display on page
+// check if resultNumber is equal to targetNumber
+
+// 	if euqual to wins go up
+// 		update wins value and diplay on page
+// 		generate new targetNumber and gem value
+// 		update html
+// 	if greater than losses go up
+// 		update loses value and diplay on page
+// 		generate new targetNumber and gem value
+// 		update html
+		
+// 	keep checking if less than
